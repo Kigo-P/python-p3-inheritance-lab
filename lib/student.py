d4b88dbf4b702408b13b37e94cd005783357f149
Kigo-P <peterkigo33@gmail.com>
2024-09-04 11:59:01 +0300

    used inheriance

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+    # initializing the student class
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+        
     
-    def learn(self):
+    def learn(self, student_knowledge):
+        self.knowledge.append(student_knowledge)
         pass
--- lib/teacher.py
+++ lib/teacher.py
@@ -3,8 +3,28 @@
 from user import User
 
 import random
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 
 class Teacher(User):
+    
+    #initializing the Teachers class
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
+    #random.randit(minimum number, maximum number)- Return random integer in range [a, b], including both end points.
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(knowledge) -1)]
+        
+
+
